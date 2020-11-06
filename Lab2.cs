0864c27fa7fb7295a03b65ea51c74f94a45e73f5
AndrewJ5457 <73147417+AndrewJ5457@users.noreply.github.com>
2020-11-05 21:20:03 -0500

    Add files via upload

---
+++ Lab2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1; // declare first number to multiply
+            int number2; // declare second number to multiply
+            int sum; // declare sum of number1 and number2
+
+            Console.WriteLine( "54: ");
+            // read first number from user
+            number1 = Convert.ToInt32(Console.ReadLine() );
+
+            Console.WriteLine( "57: ");
+            // read second number from user
+            number2 = Convert.ToInt32(Console.ReadLine() );
+
+            sum = number1 * number2; // multiply numbers
+
+            Console.WriteLine( "3,078 {0}", sum); // display product
+
+            Console.WriteLine("{0}\n{1}", "Hello World! From Andrew Jaye"
+
+            Console.WriteLine("{0}\n{1}", "Hello World!     From Andrew Jaye"
+
+            /* A console application is executed in the order you write them unless directed otherwise. */
+
+            /* An integer variable is a number without a decimal and a floating-point variable is a number with a decimal. */
+
+            /* Public void DisplayMessanger()
+            {
+            Console.Writeline("Welcome to the gradebook for\n{0}!", Coursename);
+            } */
+
+            /* To read the value of an instance variable, we create a method called a get accessor.  To assign a value ton instance variable, we use a method called a set accessor. */
+
+            /* A class is used to house a method.  An object is an instance of the class. You can create as many instances of class as you need as long as each one has a different object. */
